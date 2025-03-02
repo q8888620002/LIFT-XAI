6a3bf76c77ac63b48f66b394a9dc663abc3ecba7
Mingyu Lu <mingyulu@l2lambda.cs.washington.edu>
2025-03-01 16:25:33 -0800

    update single cohort analysis

--- single_cohort_analysis.py
+++ single_cohort_analysis.py
@@ -5,11 +5,11 @@ import pickle
 
 import numpy as np
 import torch
+import wandb
 from captum.attr import ShapleyValueSampling
 
 import src.CATENets.catenets.models.torch.pseudo_outcome_nets as pseudo_outcome_nets
 from src.dataset import Dataset
-from src.utils import *
 
 DEVICE = "cuda:1"
 
@@ -32,31 +32,83 @@ def compute_shap_values(model, data_sample, data_baseline):
     return shap_values
 
 
+def parse_args():
+    """Parser for arguments"""
+    parser = argparse.ArgumentParser(description="Single Cohort SHAP Analysis")
+    parser.add_argument(
+        "--num_trials",
+        help="number of runs ",
+        required=True,
+        type=int,
+    )
+    parser.add_argument(
+        "--cohort_name",
+        help="name of cross cohort analysis ",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "--baseline",
+        help="whether using baseline",
+        default=True,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--wandb",
+        help="whether using baseline",
+        default=True,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--relative_change_threshold",
+        help="Threshold for stopping based on local SHAP relative change",
+        default=0.05,
+        type=float,
+    )
+    return parser.parse_args()
+
 
 def main(args):
     """Main function for computing shapley value"""
 
-    trials = args["num_trials"]
-    bshap = args["baseline"]
-    cohort_name = args["cohort_name"]
+    print(args)
 
-    print("Baselines shapley:", bshap)
+    if args.wandb:
 
-    dataset1 = Dataset(args.dataset, 0)
+        wandb.init(
+            project=f"Convergence for Shapley value {args.cohort_name}",
+            notes=f"Experiment for {args.cohort_name};{args.num_trials}",
+            dir="/data/mingyulu/wandb",
+            config={
+                "num_trials": args.num_trials,
+                "dataset": args.cohort_name,
+                "relative_change_threshold": args.relative_change_threshold,
+                "model": "XLearner",
+                "baseline": args.baseline,
+            },
+        )
 
-    cohort1_predict_results = np.zeros((trials, len(cohort1_x)))
-    cohort1_average_shap = np.zeros((trials, cohort1_x.shape[0], cohort1_x.shape[1]))
+    save_path = f"results/{args.cohort_name}/shapley"  # Define the save directory
 
-    for i in range(trials):
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    dataset = Dataset(args.cohort_name, 0)
+    x_train, w_train, y_train = dataset.get_data()
+
+    cohort_predict_results = []
+    cohort_shap_values = []
+
+    for i in range(args.num_trials):
         # Model training
 
         sampled_indices = np.random.choice(
-            len(cohort1_x), size=len(cohort1_x), replace=True
+            len(x_train), size=int(0.9 * len(x_train)), replace=False
         )
 
-        x_sampled = cohort1_x[sampled_indices]
-        y_sampled = cohort1_y[sampled_indices]
-        w_sampled = cohort1_w[sampled_indices]
+        x_sampled = x_train[sampled_indices]
+        y_sampled = y_train[sampled_indices]
+        w_sampled = w_train[sampled_indices]
 
         model = pseudo_outcome_nets.XLearner(
             x_sampled.shape[1],
@@ -72,68 +124,79 @@ def main(args):
 
         model.fit(x_sampled, y_sampled, w_sampled)
 
-        cohort1_predict_results[i] = (
-            model.predict(X=cohort1_x).detach().cpu().numpy().flatten()
-        )
-        cohort2_predict_results[i] = (
-            model.predict(X=cohort2_x).detach().cpu().numpy().flatten()
+        cohort_predict_results.append(
+            model.predict(X=x_train).detach().cpu().numpy().flatten()
         )
 
-        if bshap:
-            baseline = cohort1_x.mean(0)
+        if args.baseline:
+            baseline = np.median(x_sampled, 0)
 
-            for _, idx_lst in dataset1.discrete_indices.items():
+            for _, idx_lst in dataset.discrete_indices.items():
                 if len(idx_lst) == 1:
-
                     # setting binary vars to 0.5
-
                     baseline[idx_lst] = 0.5
                 else:
                     # setting categorical baseline to 1/n
-                    # category_counts = data[:, idx_lst].sum(axis=0)
-                    # baseline[idx_lst] = category_counts / category_counts.sum()
-
-                    baseline[idx_lst] = 1 / len(idx_lst)
+                    category_counts = x_sampled[:, idx_lst].sum(axis=0)
+                    baseline[idx_lst] = category_counts / category_counts.sum()
         else:
-            baseline_index = np.random.choice(len(cohort1_x), 1)
-            baseline = cohort1_x[baseline_index]
+            baseline_index = np.random.choice(len(x_train), 1)
+            baseline = x_train[baseline_index]
 
-        cohort1_average_shap[i] = compute_shap_values(model, cohort1_x, baseline)
+        print(f"Trial {i+1}/{args.num_trials} - Computing SHAP values")
 
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+        # Compute SHAP values first
+        shap_values = compute_shap_values(model, x_train, baseline)
+        cohort_shap_values.append(shap_values)
+
+        shap_values_array = np.array(
+            cohort_shap_values
+        )  # Shape: (num_trials, num_samples, num_features)
+        mean_shap_values = np.mean(shap_values_array, axis=0)
+
+        # Compute relative change in mean local SHAP explanations
+        if i > 0:
+            prev_mean_shap_values = np.mean(np.array(cohort_shap_values[:-1]), axis=0)
+            relative_change = np.abs(mean_shap_values - prev_mean_shap_values) / (
+                np.abs(prev_mean_shap_values) + 1e-8
+            )
+            avg_relative_change = np.mean(relative_change)
+
+            if args.wandb:
+                wandb.log({"Trials": i + 1, "Relative Change": avg_relative_change})
+
+            print(
+                f"Trial {i+1}: Average Relative Change in Mean Local SHAP Explanations"
+                f" = {avg_relative_change:.6f}"
+            )
+
+            if avg_relative_change < args.relative_change_threshold:
+                print(
+                    f"Mean local SHAP explanations stabilized at trial {i}"
+                    f". Stopping early."
+                )
+                break
 
     with open(
-        os.path.join(save_path, f"{cohort1}_predict_results_{bshap}.pkl"), "wb"
+        os.path.join(
+            save_path, f"{args.cohort_name}_predict_results_{args.baseline}.pkl"
+        ),
+        "wb",
     ) as output_file:
-        pickle.dump(cohort1_predict_results, output_file)
+        pickle.dump(np.stack(cohort_predict_results), output_file)
 
     with open(
-        os.path.join(save_path, f"{cohort1}_shap_bootstrapped_{bshap}.pkl"), "wb"
+        os.path.join(
+            save_path, f"{args.cohort_name}_shap_bootstrapped_{args.baseline}.pkl"
+        ),
+        "wb",
     ) as output_file:
-        pickle.dump(cohort1_average_shap, output_file)
+        pickle.dump(np.stack(cohort_shap_values), output_file)
+
+    print("SHAP computation completed. Results saved to:", save_path)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Description of your program")
-    parser.add_argument(
-        "-t", "--num_trials", help="number of runs ", required=True, type=int
-    )
-    parser.add_argument(
-        "-c",
-        "--cohort_name",
-        help="name of cross cohort analysis ",
-        required=True,
-        type=str,
-    )
-    parser.add_argument(
-        "-b",
-        "--baseline",
-        help="whether using baseline",
-        default=True,
-        action="store_false",
-    )
 
-    args = vars(parser.parse_args())
-
+    args = parse_args()
     main(args)
