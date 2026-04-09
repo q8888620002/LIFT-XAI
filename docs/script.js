d28f817b87d26cc518e3cd9297a97dd108f567ad
Mingyu Lu <mingyulu@shap1.cs.washington.edu>
2026-04-09 14:54:57 -0700

    update website

--- docs/script.js
+++ docs/script.js
@@ -457,7 +457,7 @@ function createGateInputs(hypIndex) {
             </div>
             ${['is_biologically_coherent', 'is_causally_plausible', 'is_clinically_actionable', 'is_literature_backed'].includes(gate.id) ? `
                 <div class="rating-group gate-comment-group">
-                    <label class="rating-label">(optional) Additional comments for ${(gate.label.match(/^Q\d+/i) || ['this question'])[0]}</label>
+                    <label class="rating-label">optional explaation for ${(gate.label.match(/^Q\d+/i) || ['this question'])[0]}:</label>
                     <textarea id="${gate.id}-comments-${hypIndex}" placeholder="Optional explanation for your ${gate.label} rating..."></textarea>
                 </div>
             ` : ''}
